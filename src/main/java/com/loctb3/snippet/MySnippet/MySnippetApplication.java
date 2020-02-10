71441dc61ba4b96383c588d45bfe6ace613f4aea
Bảo Lộc Tống <loctong@gmail.com>
2020-02-10 13:04:41 +0700

    first commit

--- src/main/java/com/loctb3/snippet/MySnippet/MySnippetApplication.java
+++ src/main/java/com/loctb3/snippet/MySnippet/MySnippetApplication.java
@@ -2,9 +2,17 @@ package com.loctb3.snippet.MySnippet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class MySnippetApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home(){
+		return "Hello world";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MySnippetApplication.class, args);
