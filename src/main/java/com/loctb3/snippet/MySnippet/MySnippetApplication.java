841bf11da515b1c955713a5fa2f1678202df3696
Bảo Lộc Tống <loctong@gmail.com>
2020-02-10 14:26:19 +0700

    fix bug

--- src/main/java/com/loctb3/snippet/MySnippet/MySnippetApplication.java
+++ src/main/java/com/loctb3/snippet/MySnippet/MySnippetApplication.java
@@ -2,14 +2,12 @@ package com.loctb3.snippet.MySnippet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
-public class MySnippetApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MySnippetApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
