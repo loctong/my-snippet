4aa6227042f5e2daf1fa25ef98e7bdf7607c1355
Bảo Lộc Tống <loctong@gmail.com>
2020-02-10 12:58:18 +0700

    first commit

---
+++ src/main/java/com/loctb3/snippet/MySnippet/MySnippetApplication.java
@@ -0,0 +1,13 @@
+package com.loctb3.snippet.MySnippet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySnippetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySnippetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loctb3/snippet/MySnippet/MySnippetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loctb3.snippet.MySnippet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySnippetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
